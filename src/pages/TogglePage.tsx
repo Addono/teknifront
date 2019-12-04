ed42d1c2edb5d9db6714d56d9d1dea4b2ad7a2cf
Adriaan Knapen <hi@aknapen.nl>
2019-12-04 20:16:20 +0100

    :rocket: Switch to broker.hivemq.com

--- src/pages/TogglePage.tsx
+++ src/pages/TogglePage.tsx
@@ -16,7 +16,7 @@ const TogglePage: React.FC<ITogglePageProps> = () => {
     const [state, setState] = useState<{ transition: string, params: Color }>()
 
     React.useEffect(() => {
-        let client = mqtt.connect('wss://test.mosquitto.org:8081')
+        let client = mqtt.connect('wss://broker.hivemq.com:8000')
 
         client.on('connect', function () {
             client.subscribe('tek/staging/light/1/#')
