8e952b38efa0c1e600dda88635ba27b0561f0158
Adriaan Knapen <hi@aknapen.nl>
2019-12-07 19:46:59 +0100

    :lipstick: Redesign logo and add it to the title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './App.css'
 import ControlPage from './pages/ControlPage'
-import { Layout, Typography } from 'antd'
+import { Layout, Typography, Row, Col } from 'antd'
 
 const App: React.FC = () => (
   <Layout>
     <div className="App">
       <header className="App-header">
-        <Typography.Title>Teknifront</Typography.Title>
-        <ControlPage />
+        <Row>
+          <Col span={8}>
+            <img src={require('./logo.png')} style={{ height: "2em" }} />
+          </Col>
+
+          <Col span={16}>
+            <Typography.Title>Teknifront</Typography.Title>
+          </Col>
+        </Row>
       </header>
+      <Row>
+        <ControlPage />
+      </Row>
     </div>
   </Layout >
 )
